1b53a7a3e6da57c418d359cdd46cc562ab3b8939
der1x <130235333+Mark4353@users.noreply.github.com>
2025-04-06 15:44:31 +0300

    hw

--- main.js
+++ main.js
@@ -40,35 +40,36 @@ toggleUserState(users,"Mango").then(logger2);
 toggleUserState(users,"Lux").then(logger2);
 
 
-const randomIntegerFromInterval =(min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
 
-const makeTransaction = (transaction)=> {
-  const delay = randomIntegerFromInterval(200, 500);
-
-  return new Promise((resolve, reject) => {
+const randomIntegerFromInterval =(min, max) => { 
+  return Math.floor(Math.random() * (max - min + 1) + min); 
+}; 
+ 
+const makeTransaction = (transaction)=> { 
+  const delay = randomIntegerFromInterval(200, 500); 
+ 
+  return new Promise((resolve, reject) => { 
 
     setTimeout(( ) => {
-      const canProcess = Math.random() > 0.3;
+      const canProcess = Math.random() > 0.3; 
 
       if (canProcess){
-        resolve([transaction.id, delay]);
+        resolve([transaction.id, delay]); 
       }else {
-        reject(transaction.id) ;
+        reject(transaction.id) ; 
       }
 
-    }, delay);
-  });
-};
+    }, delay); 
+  }); 
+}; 
 
-const logSuccess = ([id, time]) =>{
+const logSuccess = ([id, time]) =>{ 
 
-  console.log(`Transaction ${id} processed in ${time}ms`);
+  console.log(`Transaction ${id} processed in ${time}ms`); 
 } ;
 
-const logError = (id) => {
-  console.warn(`Error processing transaction ${id}. Please try again later.` );
+const logError = (id) => { 
+  console.warn(`Error processing transaction ${id}. Please try again later.` ); 
 };
 
 /*
